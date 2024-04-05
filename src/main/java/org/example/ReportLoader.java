c4c1099b01362fd12add3f0d802efc6fd84295d5
khehelk <danilmarkov88@gmail.com>
2024-04-05 23:31:04 +0400

    fix

--- src/main/java/org/example/ReportLoader.java
+++ src/main/java/org/example/ReportLoader.java
@@ -1,13 +1,9 @@
 package org.example;
 
-import org.example.StemmerPorter;
-
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
